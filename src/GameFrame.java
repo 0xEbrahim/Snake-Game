70f2206f188f1a9051f35ac556aeee69f5eef920
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2023-08-06 14:48:18 +0300

    Update GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+        GameFrame() {
+            this.add(new GamePanel());
+            this.setTitle("Snake");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.pack();
+            this.setVisible(true);
+            this.setLocationRelativeTo(null);
+
+        }
 }
